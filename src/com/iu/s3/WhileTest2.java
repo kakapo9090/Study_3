5cdcc90a33e4bee457c092d60bc665d4ddbe1460
kakapo9090 <84508282+kakapo9090@users.noreply.github.com>
2021-07-15 09:02:29 +0900

    Update WhileTest2.java

--- src/com/iu/s3/WhileTest2.java
+++ src/com/iu/s3/WhileTest2.java
@@ -11,10 +11,36 @@ public class WhileTest2 {
 		int id = 1234;
 		int pw = 5678;
 		String menu = "1. 로그인    2. 종료";
-		boolean check = true;
+		boolean check = true;  
 		//1. 로그인 -> id, pw 입력 -> 로그인 판단
 		//	로그인 성공 -> 종료 , 로그인 실패 -> 메인매뉴로 돌아가기 
 		//2. 종료
+		
+		// ===================7.14 과제=====================
+		//로그인 성공, 로그인 안 됐거나
+		//로그인이 성공했으면 게임을 시작
+		//예) MMORPG게임
+	//최초레벨: 1, 만렙: 15
+	//최초gold: 0
+		//레벨 5 달성 시 1000gold 지급
+		//레벨 10 달성 시 2000gold 지급
+		//레벨 15 달성 시 3000gold 지급
+		
+		//*모든 몬스터의 경험치 증가량은 동일
+		//   Lv   : kills
+		// 1 -> 2 : 3마리
+		// "몬스터 1마리 사냥"
+		// "몬스터 2마리 사냥"
+		// "몬스터 3마리 사냥" -> "레벨업" 출력 및 kills 초기화
+		// 2 -> 3 : 6마리
+		// 3 -> 4 : 9마리
+		// 4 -> 5 : 12마리
+		// 5 -> 6 : 15마리
+		// ...
+		// 14 -> 15 : 42마리
+		// 15레벨 달성시 최종 레벨 & 최종 gold 출력 후 종료
+		
+		
 		while(check) {
 		System.out.println(menu);
 		int select = sc.nextInt();
@@ -26,7 +52,7 @@ public class WhileTest2 {
 				System.out.println("PW를 입력하세요.");
 				int ypw = sc.nextInt();
 					if(yid==id && ypw==pw) {
-						System.out.println("로그인 성공! 메뉴를 종료합니다");
+						System.out.println("로그인 성공!");
 						check=!check;
 					}else {
 						System.out.println("로그인 실패! 메인으로 돌아갑니다");
@@ -34,10 +60,60 @@ public class WhileTest2 {
 					}
 			}else {
 				System.out.println("메뉴를 종료합니다");
-				check=!check;
+				break;
 			}
 			}
 				
+		
+//===============게임시작 코드 위치는 여기서 부터 작성!================
+// 여태까지 배운것들을 이용해서 풀기
+		int lv=1;
+		int gold=0;
+		int bonus=1;
+		if(check==false) {
+		System.out.println("=================================");
+		System.out.println("[          게임 시작         ]");
+		System.out.println("계속하시려면 아무 키나 눌러주세요.");
+		System.out.println("=================================");
+		String go = sc.next();
+		System.out.println("=================================");
+		System.out.println("현재 레벨 : "+ lv);
+		System.out.println("현재 골드 : "+ gold);
+		
+		
+		for(lv=1; lv<15; lv++) {
+			System.out.println("=================================");
+			System.out.println("사냥을 시작합니다.");
+			System.out.println("계속하시려면 아무 키나 눌러주세요.");
+			System.out.println("=================================");
+			go = sc.next();
+			for(int kill=1; kill<=lv*3; kill++ ) {
+				System.out.println("몬스터 "+kill+"마리 사냥");
+				if(kill==lv*3) {
+					System.out.println("=================================");
+					System.out.println("===== 레벨 업! 현재 레벨 : "+(lv+1)+ " =====");
+					System.out.println("=================================");
+					bonus = bonus + 1;
+					if(bonus%5==0) {
+						gold = gold + ((1000)*(bonus/5));
+						System.out.println("===== 레벨 "+bonus+" 달성 보너스 "+((1000)*(bonus/5))+" gold 지급 ");
+						System.out.println("===== 현재 골드 : "+gold + " gold         ");
+						System.out.println("=================================");
+					}
+				}
+			}
+		}
+		if(lv==15) {	
+			System.out.println("===================================");
+			System.out.println(" ");
+			System.out.println("  최종 레벨을 달성하여 게임을 종료합니다.  ");
+			System.out.println(" ");
+			System.out.println(" ● 최종 레벨 : "+lv);
+			System.out.println(" ● 최종 gold : "+gold);
+			System.out.println(" ");
+			System.out.println("===================================");
+		}
+		}
 		System.out.println("WhileTest2 Finish");
 	}
 
